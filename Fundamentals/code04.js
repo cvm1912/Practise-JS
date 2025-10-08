6b46f6c439a1e7d3ec865b22b50b3ea11a0b55b5
cvm1912 <shivam.fcse@gmail.com>
2025-10-08 17:38:06 +0530

    conditional statements

---
+++ Fundamentals/code04.js
@@ -0,0 +1,13 @@
+// IF ELSE
+let name = "shivam";
+
+// console.log(name+ "is married")
+// console.log(name+ "is not married")
+
+
+let isMarried = false;
+if(isMarried){
+    console.log(name+ " is married")
+}else{
+    console.log(name+ " is not married")
+}
