bb9d85d06935ac24b5a8c529c2f083c405ef7570
cvm1912 <shivam.fcse@gmail.com>
2025-10-08 17:28:36 +0530

    datatypes of varaible

---
+++ Fundamentals/types.js
@@ -0,0 +1,8 @@
+let a = 1;
+a = "shivam";
+console.log(a);
+
+// step 1:  defining a variable 
+// step 2 : change the value of variabe
+
+// note : in const , you cannot change the value of variable
